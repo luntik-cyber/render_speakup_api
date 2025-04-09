7510133dc375588472a987cbf38d53b316efacb4
luntik-cyber <fordevelopment321@gmail.com>
2025-04-09 15:18:48 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+
+usernames = ["oto", "speak up", "Lasha Gorgiladze", "Oto", "Oto Dzorelashvili"]
+passwords = ["Shazam12@", "mevaryle", "elenethebest"]
+
+firebase_config = {
+    "apiKey": "AIzaSyD93WyvlqmBo_CkChloCabFARucdoDILIA",
+    "authDomain": "facebook-clone-fe696.firebaseapp.com",
+    "projectId": "facebook-clone-fe696",
+    "storageBucket": "facebook-clone-fe696.appspot.com",
+    "messagingSenderId": "172533380090",
+    "appId": "1:172533380090:web:2f80ab7f5f80d7cefdaa43",
+    "measurementId": "G-2GH8KQ7ZNX"
+}
+
+@app.route("/getConfig/<input_username>/<input_password>", methods=["GET"])
+def give_config(input_username, input_password):
+    if input_username in usernames and input_password in passwords:
+        return jsonify(firebase_config)
+    
+    return jsonify({"error": "Unauthorized"}), 401
+
+if __name__ == "__main__":
+    app.run()
